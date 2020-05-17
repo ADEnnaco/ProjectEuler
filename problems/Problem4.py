db822f8fe4a41d56c74e08f8944014999151200d
Andy Ennaco <andy.ennaco@gmail.com>
2020-05-16 22:32:07 -0400

    Changed explanatory comment format.

--- problems/Problem4.py
+++ problems/Problem4.py
@@ -5,14 +5,14 @@ A palindromic number reads the same both ways. The largest palindrome made from 
 
 Find the largest palindrome made from the product of two 3-digit numbers.
 """
+# =============================================================================
+# The largest number that is the product of two 3-digit numbers is 999^2 which
+# is 998,001. Therefore, the largest possible answer is 997,799. Starting here,
+# we generate each palindrome p, find the divisors, see if there exists a
+# divisor d such that sqrt(p) < d < 1000. If this divisor exists, and p is a 
+# 6-digit number then p / d will also be 3-digits, and we have our solution.
+# =============================================================================
 
-"""
-The largest number that is the product of two 3-digit numbers is 999^2 which is
-998,001. Therefore, the largest possible answer is 997,799. Starting here, we
-generate each palindrome p, find the divisors, see if there exists a divisor d
-such that sqrt(p) < d < 1000. If this divisor exists, and p is a 6-digit number
-then p / d will also be 3-digits, and we have our solution.
-"""
 from functions.integers import get_divisors
 
 import time
