d7742d9d7134a36dec5f68ef723b1f4a8348c25f
Andy Ennaco <andy.ennaco@gmail.com>
2020-05-27 20:59:02 -0400

    add function for enumerating combinations of digits

--- functions/combinatorics.py
+++ functions/combinatorics.py
@@ -37,3 +37,43 @@ def permutate(string):
     else:
         print("ERROR: permutate received invalid input.\nREASON: input must",
               "be a string.")
+#-----------------------------------------------------------------------------
+#-----------------------------------------------------------------------------
+def digit_combos(n, start=0, stop=9):
+    """
+    Returns all the possible unique combinations of digits of length n.
+    
+    Example: digit_combos(2) returns:
+        ['00', '01', '11', '02', '12', '22', '03', '13', '23', '33', '04',
+         '14', '24', '34', '44', '05', '15', '25', '35', '45', '55', '06',
+         '16', '26', '36', '46', '56', '66', '07', '17', '27', '37', '47', 
+         '57', '67', '77', '08', '18', '28', '38', '48', '58', '68', '78', 
+         '88', '09', '19', '29', '39', '49', '59', '69', '79', '89', '99']
+
+    Parameters
+    ----------
+    n : integer
+        The number of digits in each combination.
+    start : integer, optional (default = 0)
+        The lowest digit to consider for each combination.
+    stop : integer, optional (default = 9)
+        The highest digit to consider for each combination.
+
+    Returns
+    -------
+    combos : list of strings
+        Each possible combination, as a string to preserve leading zeros.
+        Combinations will all be given in ascending order.
+    """
+    if all(map(lambda x: isinstance(x, int), [n, start, stop])):
+        if n == 1:
+            return [str(x) for x in range(start, stop + 1)]
+        else:
+            combos = []
+            for sub_combo in digit_combos(n-1, start, stop):
+                for i in range(start, int(sub_combo[0])+1):
+                    combos.append(str(i) + sub_combo)
+            return combos
+    else:
+        print("ERROR: digit_combos received invalid input.\nREASON: n, start,",
+              "and stop must all be integers.")
